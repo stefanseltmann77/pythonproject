e663d81f010335ef51147ba24f512a0d9a1ea11f
Stefan.Seltmann <s.seltmann06@web.de>
2020-06-22 23:36:20 +0200

    adding project_root to config

--- my_project_module/conf/__init__.py
+++ my_project_module/conf/__init__.py
@@ -1,6 +1,10 @@
 import configparser
+import os
+from pathlib import Path
 
 from .config_default import default_config
+
+project_root = Path(os.path.abspath(__file__)).parents[2]
 
 config_oracle = configparser.ConfigParser()
 config_oracle.read_dict(default_config)
